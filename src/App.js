17ca01453291ab08ed47e44f0c3ed3d08da78b23
Ngo Van Tu <it.tunv@gmail.com>
2021-07-30 23:34:20 +0700

    tunv - create not found page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import CountryDetail from "./components/CountryDetail/CountryDetail";
 import News from "./components/News/News";
-
-
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -18,7 +17,7 @@ function App() {
         <PrivateRoute exact path="/" component={Home} />
         <PrivateRoute exact path="/detail/:country" component={CountryDetail} />
         <PrivateRoute exact path="/news" component={News} />
-
+        <Route component={NotFound}></Route>
       </Switch>
     </Router>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css'
+function NotFound(props) {
+    return (
+        <div className="not-found">
+            404 NOT FOUND!
+        </div>
+    );
+}
+
+export default NotFound;
