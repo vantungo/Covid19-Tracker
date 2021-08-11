d97f3fb18c77abcfe237ff0214457d0add31fc8c
Ngo Van Tu <it.tunv@gmail.com>
2021-08-12 01:16:49 +0700

    Revert "Revert "tunv - complete add country map for country detail page""
    This reverts commit f56796ef031fe8ec2a020bdf5101dab56b3f6ddb.

--- src/components/CountryDetail/NationalFlag/NationalFlag.js
+++ src/components/CountryDetail/NationalFlag/NationalFlag.js
@@ -4,10 +4,7 @@ import { Typography, Box } from "@material-ui/core";
 function NationalFlag({ countryInfo, countryDetail }) {
   return (
     <Box style={{ textAlign: "center", marginBottom: "2em" }}>
-      <Typography gutterBottom variant="h3" color="textSecondary">
-        {countryDetail.country}
-      </Typography>
-      <img src={countryInfo.flag} alt="" />
+      <img src={countryInfo.flag} alt="" style={{width:"300px"}} />
     </Box>
   );
 }
