58b0fa7771e9b1dc220814f4d18510faba41a1f0
Ngo Van Tu <it.tunv@gmail.com>
2021-08-12 00:59:37 +0700

    Revert "tunv - complete add country map for country detail page"

--- src/components/CountryDetail/CountryHighlight/CountryHighlight.js
+++ src/components/CountryDetail/CountryHighlight/CountryHighlight.js
@@ -4,8 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { useTranslation } from "react-i18next";
-import CountUp from "react-countup";
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles({
   root: {
@@ -33,12 +32,13 @@ function CountryHighlight({ countryDetail }) {
         <Grid item sm={4} xs="12">
           <Card className={classes.root} variant="outlined">
             <CardContent>
-              <Typography className={classes.title} gutterBottom>
+              <Typography
+                className={classes.title}
+                gutterBottom
+              >
                 {t("Confirmed cases.1")}
               </Typography>
-              <Typography variant="h6">
-                <CountUp end={countryDetail.cases} separator=" " duration={3} />
-              </Typography>
+              <Typography variant="h6">{countryDetail.cases}</Typography>
             </CardContent>
           </Card>
         </Grid>
@@ -46,16 +46,13 @@ function CountryHighlight({ countryDetail }) {
         <Grid item sm={4} xs="12">
           <Card className={classes.root} variant="outlined">
             <CardContent>
-              <Typography className={classes.title} gutterBottom>
+              <Typography
+                className={classes.title}
+                gutterBottom
+              >
                 {t("Deaths.1")}
               </Typography>
-              <Typography variant="h6">
-                <CountUp
-                  end={countryDetail.deaths}
-                  separator=" "
-                  duration={3}
-                />
-              </Typography>
+              <Typography variant="h6">{countryDetail.deaths}</Typography>
             </CardContent>
           </Card>
         </Grid>
@@ -63,16 +60,13 @@ function CountryHighlight({ countryDetail }) {
         <Grid item sm={4} xs="12">
           <Card className={classes.root} variant="outlined">
             <CardContent>
-              <Typography className={classes.title} gutterBottom>
+              <Typography
+                className={classes.title}
+                gutterBottom
+              >
                 {t("Recovered.1")}
               </Typography>
-              <Typography variant="h6">
-                <CountUp
-                  end={countryDetail.recovered}
-                  separator=" "
-                  duration={3}
-                />
-              </Typography>
+              <Typography variant="h6">{countryDetail.recovered}</Typography>
             </CardContent>
           </Card>
         </Grid>
